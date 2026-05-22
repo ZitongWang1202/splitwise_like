85b998f44d2e1ee2dd9438edbfb75f9f7aafc27f
ZitongWang1202 <zitongw961202@gmail.com>
2026-05-22 15:34:35 -0400

    fix Ruff

--- backend/create_tables.py
+++ backend/create_tables.py
@@ -1,13 +1,13 @@
 from app.db.base import Base
 from app.db.session import engine
 
-from app.models.user import User
-from app.models.group import Group
-from app.models.group_member import GroupMember
-from app.models.expense import Expense
-from app.models.expense_participant import (
-    ExpenseParticipant
-)
+# from app.models.user import User
+# from app.models.group import Group
+# from app.models.group_member import GroupMember
+# from app.models.expense import Expense
+# from app.models.expense_participant import (
+#     ExpenseParticipant
+# )
 
 Base.metadata.create_all(bind=engine)
 
