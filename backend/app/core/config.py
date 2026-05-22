e2024673e69162e81687a6f4c590b9d5a7503051
ZitongWang1202 <zitongw961202@gmail.com>
2026-05-22 14:42:40 -0400

    edit .env

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,6 +8,10 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
 
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
 
     class Config:
         env_file = ".env"
