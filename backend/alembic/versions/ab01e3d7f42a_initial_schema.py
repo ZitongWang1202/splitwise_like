5debebf6ae3587aa2f9f725a1a472800b0f4fc64
ZitongWang1202 <zitongw961202@gmail.com>
2026-05-22 15:37:22 -0400

    fix Ruff: alembic

--- backend/alembic/versions/ab01e3d7f42a_initial_schema.py
+++ backend/alembic/versions/ab01e3d7f42a_initial_schema.py
@@ -7,8 +7,8 @@ Create Date: 2026-05-19 11:18:40.172699
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
+# from alembic import op
+# import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
