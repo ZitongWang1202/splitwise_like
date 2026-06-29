e899ad2a84fa258349160544dd0c9403b7874c35
ZitongWang1202 <zitongw961202@gmail.com>
2026-06-29 19:46:22 -0400

    Fix frontend Dockerfile

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
 
-    CORS_ORIGINS: str = "http://localhost:5173,http://localhost:4173,https://splitwise-frontend-8p87.onrender.com"
+    CORS_ORIGINS: str = "http://localhost:5173,http://localhost:4173,https://splitwise-frontend-60c3.onrender.com"
 
     model_config = SettingsConfigDict(
         env_file=".env"
